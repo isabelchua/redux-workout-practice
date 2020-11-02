d967cc2aab2da87d5b7d2be5f13b72c44ed67f1e
isabelchua <isabelchua86@gmail.com>
2020-11-02 13:44:04 -0800

    added activity component

---
+++ src/components/Workouts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Activity from "./Activity";
+
+const Workouts = () => {
+	return (
+		<div>
+			<h2>Workouts</h2>
+			<Activity />
+		</div>
+	);
+};
+
+export default Workouts;
